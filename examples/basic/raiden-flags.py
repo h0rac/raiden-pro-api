fe89f80cca8a339d63dcb322eeb9013cae42cad7
Adam Laurie <adam@algroup.co.uk>
2025-07-22 18:13:20 +0200

    add gpio and cycling reset line to flags demo

--- examples/basic/raiden-flags.py
+++ examples/basic/raiden-flags.py
@@ -30,12 +30,15 @@ def showflags():
     print("  Glitched: %d" % raiden.is_glitched());
     print("  Finished: %d" % raiden.is_finished());
     print("  Power: %d" % raiden.glitch_out());
+    print("  GPIO1_in: %d" % raiden.is_gpio1_in_high());
+    print("  GPIO2_in: %d" % raiden.is_gpio2_in_high());
+    print("  GPIO_out: %d" % raiden.gpio_out_status());
 
 print(raiden.get_buildtime())
 
 print("clearing flags")
 print("")
-raiden.reset_fpga()
+raiden.reset_glitcher()
 raiden.arm(0)
 raiden.set_target_power("off")
 showflags()
@@ -50,3 +53,18 @@ print("power on...")
 raiden.set_target_power("on")
 showflags()
 
+print("")
+print("gpio_out on...")
+raiden.set_param(param="CMD_GPIO_OUT", value= True)
+showflags()
+
+print("")
+print("toggling gpio_out")
+raiden.set_param(param="CMD_GPIO_OUT", value= False)
+time.sleep(1)
+raiden.set_param(param="CMD_GPIO_OUT", value= True)
+print("done")
+print("CTL-C to exit")
+
+while 42:
+    pass
